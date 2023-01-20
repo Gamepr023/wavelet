0715b840ad331e95fd6ede7afcc87ab85f2fb0c6
Gamepr023 <122562465+Gamepr023@users.noreply.github.com>
2023-01-19 18:01:15 -0800

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,52 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+public class SearchEngine implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+    ArrayList<String> searchList = new ArrayList<String>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Allen's Number: %d", num);
+        } else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("count")) {
+                    num += Integer.parseInt(parameters[1]);
+                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                }
+                else if (parameters[0].equals("s")) {
+                    searchList.add(parameters[1]);
+                    
+                    return String.format("%s has been added.\n", parameters[1],);
+                }
+            }
+            if (url.getPath().contains("/search")) {
+                String[] parameters = url.getQuery().split("=");
+                for (int i = 0; i < searchList.size(); i++) {
+                    
+                }
+            }
+            return "404 Not Found!";
+        }
+    }
+        
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new SearchEngine());
+    }
+}
+
